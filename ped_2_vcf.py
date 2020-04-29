8396f52f92a8af1c72182cf801580cfeee9c3286
Ruhollah Shemirani <Roohy@Ruhollahs-MacBook-Pro.local>
2020-04-28 18:10:25 -0700

    ped2vcf

---
+++ ped_2_vcf.py
@@ -0,0 +1,39 @@
+import sys,os
+import numpy as np
+from reader import load_map_data
+
+lines = ['##fileformat=VCFv4.2',
+            '##fileDate=20200429',
+            '##source=Roohyv2.00',
+            '##contig=<ID=1,length=247189074>',
+            '##INFO=<ID=PR,Number=0,Type=Flag,Description="Provisional reference allele, may not be based on real reference genome">',
+            '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">']
+fields = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT']
+
+if __name__ == '__main__':
+    pedAddr = sys.argv[1]
+    mapAddr = sys.argv[2]
+    vcfAddr = sys.argv[3]
+    sampleCount = sys.argv[4]
+    mapData,posDict = load_map_data(mapAddr)
+    IDList = []
+    SNPCount = len(mapData)
+    haps = np.zeros((SNPCount,sampleCount),dtype=np.dtype('U3'))
+    
+    with open(pedAddr,'r') as pedFile:
+        for i in range(sampleCount):
+            sample = pedFile.readline()
+            data = sample.strip().split()
+            IDList.append(data[1])
+            for j in range(SNPCount):
+                haps[j,i] = data[6+(j*2)]+'/'+data[7+(j*2)]
+    with open(vcfAddr,'w') as vcfFile:
+        vcfFile.write('\n'.join(lines)+'\n')
+        vcfFile.write('\t'.join(fields+IDList)+'\n')
+        for i in range(SNPCount):
+            vcfFile.write('\t'.join([str(mapData[i][0]),str(mapData[i][3]),
+                mapData[i][1],'C','A','.','.','PR','GT'])+'\t')
+            vcfFile.write('\t'.join(haps[i,:])+'\n')
+    
+        
+
