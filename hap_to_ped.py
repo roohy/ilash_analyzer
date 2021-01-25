d926d6ef94de154b6d2dc0306c4c5ace121e4220
Ruhollah Shemirani <shemirani.r@gmail.com>
2021-01-25 11:50:32 -0800

    improved hap_to_ped

--- hap_to_ped.py
+++ hap_to_ped.py
@@ -21,10 +21,8 @@ def convert_haps(hapAddr,size,dim,outputAddr):
             prepended = '%05d' % i
             output.write('0 ' + prepended + ' 0 0 0 -9 ')
             for j in range((haps.shape[1])-1):
-                output.write(haps[2*i,j]+' ')
-                output.write(haps[2*i +1,j]+' ')
-            output.write(haps[haps.shape[0]-2,haps.shape[1]-1]+' ')
-            output.write(haps[haps.shape[0]-1,haps.shape[1]-1]+'\n')
+                output.write('{} {} '.format(haps[2*i,j],haps[2*i +1,j]))
+            output.write('{} {}\n'.format(haps[2*i,haps.shape[1]-1],haps[2*i+1,haps.shape[1]-1]))
 
 if __name__ == '__main__':
     inputAddr = sys.argv[1]
